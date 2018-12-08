523e9761ca373b12e5680bd6d1e482201837ab0d
e185760 <e185760@ie.u-ryukyu.ac.jp>
2018-12-09 06:42:02 +0900

    Report4

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+//        String str =null;
+//        try{
+//            System.out.println(str.length());
+//        }catch(NullPointerException error){
+//            System.out.println("エラーの種類: NullPointerException");
+//            System.out.println(error.getMessage());
+//        }
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+    }
+}
